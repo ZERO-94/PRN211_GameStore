922771ec1f0c7dab222e8f6544e7ce8590bdcf97
Trong Nhan <nhantrongho2002@gmail.com>
2022-04-20 17:22:57 +0700

    Add GameDAO, CateGoryDAO, GameLicenseDAO, UserDAO

--- DataAccess/CategoryDAO.cs
+++ DataAccess/CategoryDAO.cs
@@ -1,8 +1,102 @@
-﻿using System;
-
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 public class CategoryDAO
 {
-	public CategoryDAO()
-	{
-	}
+    private static CategoryDAO instance;
+    private static readonly object instanceLock = new object();
+    private GameStoreDBContext context = new GameStoreDBContext();
+    public CategoryDAO()
+    {
+    }
+    public static CategoryDAO Instance
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                {
+                    instance = new CategoryDAO();
+                }
+
+                return instance;
+            }
+
+        }
+    }
+
+    public List<Category> GetAllCategories() => context.Categories.ToList<Category>();
+    public Category GetCategoryById(string id) => context.Categories.SingleOrDefault<Category>((c) => c.Id == id);
+
+    public bool CreateCategory(Category newCategory)
+    {
+        try
+        {
+            context.Categories.Add(newCategory);
+            context.SaveChanges();
+            return true;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return false;
+        }
+        catch (DbUpdateException ex)
+        {
+            return false;
+        }
+    }
+
+    public bool DeleteCategory(string id)
+    {
+        try
+        {
+            Category deletedCategory = GetCategoryById(id);
+            if (deletedCategory != null)
+            {
+                context.Categories.Remove(deletedCategory);
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return false;
+        }
+        catch (DbUpdateException ex)
+        {
+            return false;
+        }
+    }
+
+    public bool UpdateCategory(string id, Category updatedMemberInfo)
+    {
+        try
+        {
+            Category updateCategory = GetCategoryById(id);
+            if (updateCategory != null)
+            {
+                context.Categories.Update(updatedMemberInfo);
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return false;
+        }
+        catch (DbUpdateException ex)
+        {
+            return false;
+        }
+    }
 }
--- DataAccess/GameLicenseDAO.cs
+++ DataAccess/GameLicenseDAO.cs
@@ -1,8 +1,52 @@
-﻿using System;
+﻿using BusinessObject.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 public class GameLicenseDAO
 {
-	public GameLicenseDAO()
-	{
-	}
+    private static GameLicenseDAO instance;
+    private static readonly object instanceLock = new object();
+    private GameStoreDBContext context = new GameStoreDBContext();
+    public GameLicenseDAO()
+    {
+    }
+    public static GameLicenseDAO Instance
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                {
+                    instance = new GameLicenseDAO();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public List<GameLicense> GetAllGameLicenses() => context.GameLicenses.ToList<GameLicense>();
+
+    public List<GameLicense> GetGameLicensesListByUserID(string userID, List<GameLicense> gameLicenses)
+    {
+        return gameLicenses.FindAll(g => g.UserId == userID);
+    }
+    public GameLicense GetGameLicenseByUserIDAndGameID(string userId, string gameId) => context.GameLicenses.SingleOrDefault<GameLicense>((g) => g.UserId == userId && g.GameId == gameId);
+
+    public bool DeactivateGameLicense(string userId, string gameId)
+    {
+        GameLicense deactivateGameLicense = GetGameLicenseByUserIDAndGameID(userId, gameId);
+        if (deactivateGameLicense != null)
+        {
+            deactivateGameLicense.Status = false;
+            context.GameLicenses.Update(deactivateGameLicense);
+            context.SaveChanges();
+            return true;
+        }
+        return false;
+
+    }
+
 }
+s
